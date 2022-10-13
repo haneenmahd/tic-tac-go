d369c46accef9b6cdfd0d555676b4e154de9d4c5
Haneen Mahdin <haneenmahdin@gmail.com>
2022-10-13 19:03:18 +0530

    update import path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import AppWraper from "./App";
-import Home from "./pages";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
