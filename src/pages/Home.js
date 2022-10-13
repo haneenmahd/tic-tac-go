a0857fb57076d1afad32ce2bc37a3bd118cc8f58
Haneen Mahdin <haneenmahdin@gmail.com>
2022-10-13 19:57:24 +0530

    add layout fo r home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,32 @@
 import * as React from "react";
+import { Button, COLORS, FlexDiv } from "../styling";
+import SymbolO from "../assets/svg/O-symbol.svg";
+import SymbolX from "../assets/svg/X-symbol.svg";
 
-class Home extends React.Component {
-  render() {
-    return <div>Home</div>;
-  }
-}
+const Home = () => {
+  return (
+    <FlexDiv flexHeight>
+      <FlexDiv direction="row">
+        <img src={SymbolX} alt="X symbol" />
+        <img src={SymbolO} alt="O symbol" />
+      </FlexDiv>
+
+      <FlexDiv direction="column">
+        <h2
+          style={{
+            color: COLORS.blue,
+          }}
+        >
+          Choose your play mode
+        </h2>
+
+        <FlexDiv direction="column" gap="20px">
+          <Button>With AI</Button>
+          <Button secondary>With a friend</Button>
+        </FlexDiv>
+      </FlexDiv>
+    </FlexDiv>
+  );
+};
 
 export default Home;
