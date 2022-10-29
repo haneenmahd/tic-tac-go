ab87509e224c49f11ba58552ce29a5f6251abd64
Haneen Mahdin <haneenmahdin@gmail.com>
2022-10-29 17:45:20 +0530

    add divider and colors

--- client/src/styling/index.js
+++ client/src/styling/index.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const QUERIES = {
   large: "min-width: 801px",
@@ -8,6 +8,7 @@ export const COLORS = {
   black: "#000",
   hoverTextBlack: "#00000050",
   gray: "#8B8B8B",
+  fadedGray: "#ececec",
   lightGray: "#dfdfdf",
   white: "#fff",
   hoverBackground: "#00000010",
@@ -16,6 +17,7 @@ export const COLORS = {
 export const TRANSITIONS = {
   hovers: "150ms cubic-bezier(0.72, 0.04, 0.04, 1.01)",
   focus: "200ms cubic-bezier(0.72, 0.04, 0.04, 1.01)",
+  load: "cubic-bezier(0.69, -0.03, 0.2, 1)",
 };
 
 export const FlexDiv = styled.div`
@@ -24,4 +26,14 @@ export const FlexDiv = styled.div`
   align-items: ${(props) => props.alignment || "center"};
   justify-content: ${(props) => props.justify || "center"};
   gap: ${(props) => props.gap};
+`;
+
+export const Divider = styled.div`
+  width: 405px;
+  height: 0px;
+  border: 1px solid ${COLORS.fadedGray};
+`;
+
+export const PadBox = styled.div`
+  padding: ${(props) => props.padding};
 `;
