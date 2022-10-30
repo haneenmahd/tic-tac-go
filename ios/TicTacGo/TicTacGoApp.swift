eb8e5c33410e74cd412eaea980af524edc0dfe16
Haneen Mahdin <haneenmahdin@gmail.com>
2022-10-30 16:13:50 +0530

    add ios and web

---
+++ ios/TicTacGo/TicTacGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacGoApp.swift
+//  TicTacGo
+//
+//  Created by Haneen Mahdin on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
