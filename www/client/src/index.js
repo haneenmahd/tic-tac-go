94792b39d42360ed16616117572d2e7f623782c4
Haneen Mahdin <haneenmahdin@gmail.com>
2022-11-01 16:59:11 +0530

    remove the usage of context

--- www/client/src/index.js
+++ www/client/src/index.js
@@ -5,44 +5,31 @@ import "./index.css";
 import AppWraper from "./AppWrapper";
 import Home from "./pages/Home";
 import Play from "./pages/Play";
-import Game from "./pages/Game";
-import UserProvider from "./context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            index
-            element={
-              <AppWraper>
-                <Home />
-              </AppWraper>
-            }
-          />
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          index
+          element={
+            <AppWraper>
+              <Home />
+            </AppWraper>
+          }
+        />
 
-          <Route
-            path="/play"
-            element={
-              <AppWraper>
-                <Play />
-              </AppWraper>
-            }
-          />
-
-          <Route
-            path="/game"
-            element={
-              <AppWraper>
-                <Game />
-              </AppWraper>
-            }
-          />
-        </Routes>
-      </Router>
-    </UserProvider>
+        <Route
+          path="/play"
+          element={
+            <AppWraper>
+              <Play />
+            </AppWraper>
+          }
+        />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
