00941a5274f7a7036c6388684c876a49d3e19681
Haneen Mahdin <haneenmahdin@gmail.com>
2022-11-29 00:55:15 +0530

    fix: sending game updates to all players

--- www/server/src/controllers/GameController.ts
+++ www/server/src/controllers/GameController.ts
@@ -8,7 +8,7 @@ class GameController {
         gameRoom: string,
         message: UpdateGameMessage
     ) {
-        socket.to(gameRoom).emit("on_game_update", message);
+        socket.broadcast.to(gameRoom).emit("on_game_update", message);
     }
 }
 
