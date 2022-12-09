475d533883972a44597b5c37c777a47abb2d6793
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-09 23:52:51 +0530

    chore: remove unused parameters

--- www/server/src/controllers/MainController.ts
+++ www/server/src/controllers/MainController.ts
@@ -12,7 +12,7 @@ export default class MainController {
     ) {
         socket.on("join_game", message => this.roomController.joinGame(socket, message));
         socket.on("join_room", room => this.roomController.joinRoom(socket, room));
-        socket.on("update_game", message => this.gameController.updateGame(socket, io, this.roomController.roomId!, message));
+        socket.on("update_game", message => this.gameController.updateGame(socket, this.roomController.roomId!, message));
     }
 
     public onDisconnect(
