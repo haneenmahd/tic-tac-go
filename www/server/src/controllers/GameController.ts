56d3d9e31be9d37cbb554bc3acfb1a764a0d02aa
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-12 19:15:18 +0530

    feat: add `next-round`

--- www/server/src/controllers/GameController.ts
+++ www/server/src/controllers/GameController.ts
@@ -1,5 +1,5 @@
 import { Server, Socket } from "socket.io";
-import type { UpdateGameMessage, UpdateScoreMessage } from "src/types";
+import type { NextRoundMessage, UpdateGameMessage, UpdateScoreMessage } from "src/types";
 
 class GameController {
     public async updateGame(
@@ -10,14 +10,20 @@ class GameController {
         socket.broadcast.to(gameRoom).emit("on_game_update", message);
     }
 
-    // when you are recieving a score update it is a opponent's score update
-    // if you are sending you are updating your score.
     public async updateScore(
         socket: Socket,
         gameRoom: string,
         message: UpdateScoreMessage
     ) {
         socket.broadcast.to(gameRoom).emit("on_game_score_update", message);
+    }
+
+    public async nextRound(
+        socket: Socket,
+        gameRoom: string,
+        message: NextRoundMessage
+    ) {
+        socket.broadcast.to(gameRoom).emit("on_next_round", message);
     }
 
     public async clearGame(
--- www/server/src/types.ts
+++ www/server/src/types.ts
@@ -28,3 +28,7 @@ export interface UpdateGameMessage {
 export interface UpdateScoreMessage {
     score: number;
 }
+
+export interface NextRoundMessage {
+    round: number;
+}
