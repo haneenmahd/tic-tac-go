d9b3e2be3069e0ee39ddb6ceba02d38170486dbc
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-15 17:19:42 +0530

    refactor: split game actions in to another component

---
+++ www/client/src/components/GameActions.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { COLORS } from '../styling';
+import Button from './Button';
+
+
+const GameActionsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 29px;
+  transition: opacity 200ms;
+
+  ${p => p.disabled && css`
+    opacity: 0.5;
+    pointer-events: none;
+  `}
+`;
+
+const GameActionButton = styled(Button)`
+  font-weight: 500;
+  padding: 0 20px;
+  color: ${COLORS.black};
+  background: ${COLORS.blue};
+
+  &:hover {
+    background: ${COLORS.secondaryHoverBackground};
+  }
+`;
+
+const GameActionButtonSecondary = styled(GameActionButton)`
+  font-weight: normal;
+  border: 1px solid ${COLORS.blue};
+  background: ${COLORS.fadedBlue};
+
+  &:hover {
+    background: ${COLORS.blue}90;
+  }
+`;
+
+const GameActions = ({
+    disabled,
+    primaryAction,
+    secondaryAction,
+    primaryIcon,
+    secondaryIcon
+}) => {
+    return (
+        <GameActionsContainer disabled={disabled}>
+            <GameActionButton onClick={primaryAction}>
+                Next Round
+
+                {primaryIcon}
+            </GameActionButton>
+
+            <GameActionButtonSecondary onClick={secondaryAction}>
+                Chat
+
+                {secondaryIcon}
+            </GameActionButtonSecondary>
+        </GameActionsContainer>
+    );
+}
+
+export default GameActions;
