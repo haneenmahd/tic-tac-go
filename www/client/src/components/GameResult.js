1c786786d59ea2ecfbdb200e190608982d745682
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-16 17:58:32 +0530

    feat: add game result fading animation and fix height of the view

--- www/client/src/components/GameResult.js
+++ www/client/src/components/GameResult.js
@@ -1,18 +1,29 @@
-import Avatar from 'boring-avatars';
 import React from 'react';
-import styled from 'styled-components';
-import { COLORS, Divider } from '../styling';
+import styled, { keyframes } from 'styled-components';
+import { COLORS, Divider, TRANSITIONS } from '../styling';
+import Avatar from 'boring-avatars';
 import GameActions from './GameActions';
 import ArrowRight from "../assets/svg/icons/arrow-right.svg";
+
+const FadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
+    }
+`;
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
+    min-height: calc(100vh - 150px);;
     max-width: 100vw;
     padding: 3rem;
+    animation: ${FadeIn} 1s ${TRANSITIONS.load};
 `;
 
 const HeaderContainer = styled.div`
@@ -63,13 +74,13 @@ const GameResult = ({
 }) => {
     const didPlayerWin = playerScore > opponentScore ? true : playerScore < opponentScore ? false : null;
     const title = didPlayerWin ? "Congratulations!" : didPlayerWin === false ? "Bad luck :(" : "Well Played!";
-    const description = (playerWon, playerLost) => {
-        if (playerWon === playerName) {
+    const description = (playerWon) => {
+        if (!playerWon) {
+            return "Both of you have won the game. It was competitive and a great one!";
+        } else if (playerWon === playerName) {
             return "You won the game. Well Played!";
         } else if (playerWon !== playerName) {
             return `${playerWon} has won the game. But well played! Good luck with your next game.`;
-        } else {
-            return "Both of you have won the game. It was competitive and a great one!";
         }
     };
 
@@ -80,8 +91,7 @@ const GameResult = ({
                 <h3>
                     {
                         description(
-                            didPlayerWin ? playerName : opponentName,
-                            didPlayerWin ? opponentName : playerName
+                            didPlayerWin ? playerName : didPlayerWin === false ? opponentName : null
                         )
                     }
                 </h3>
