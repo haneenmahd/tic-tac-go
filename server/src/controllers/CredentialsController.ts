16a4357675de480126dafa29782310526ce1f72a
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-27 00:10:32 +0530

    refactor: switch to nextjs

--- server/src/controllers/CredentialsController.ts
+++
@@ -1,7 +0,0 @@
-import crypto from "crypto";
-
-class CredentialsController {
-    static generateId = () => crypto.randomBytes(5).toString();
-}
-
-export default CredentialsController;
--- server/src/handlers/onListening.ts
+++
@@ -1,5 +0,0 @@
-import type { HTTPPort } from "src/types";
-
-export default function onListening(port: HTTPPort) {
-    console.log("Listening on port", port);
-}
