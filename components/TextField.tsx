1ff2ef5b4e1a3e017d08142ce33d40e3792c1bdc
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-28 17:27:02 +0530

    fix: import path for component constants

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { COLORS, TRANSITIONS } from "../styling";
+import { COLORS, TRANSITIONS } from "./constants";
 
 const TextField = styled.input`
   width: 100%;
