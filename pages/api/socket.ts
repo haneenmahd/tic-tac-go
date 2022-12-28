6788ad02fd6e4e583bcb8592cd92be7b2e7d9fc4
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-28 17:54:39 +0530

    fix: socket can't connect different players

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -11,18 +11,22 @@ export type SocketResponse = NextApiResponse & {
 };
 
 export default function handler(req: NextApiRequest, res: SocketResponse) {
-    if (!res.socket!.server.io) {
-        const io = new Server(res.socket!.server as Partial<ServerOptions>);
-        res.socket!.server.io = io;
-
-        const mainController = new MainController();
-
-        io.on("connection", socket => {
-            mainController.onConnection(socket, io);
-
-            mainController.onDisconnect(socket);
-        });
+    if (res.socket.server.io) {
+        res.end();
+        return;
     }
+
+    const io = new Server(res.socket.server as Partial<ServerOptions>);
+    res.socket.server.io = io;
+
+    const mainController = new MainController();
+
+    // Define actions inside
+    io.on("connection", socket => {
+        mainController.onConnection(socket, io);
+
+        mainController.onDisconnect(socket);
+    });
 
     res.end();
 }
