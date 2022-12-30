2e5224cca7cd2cd8375c7cb247f904d5c94765ea
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-30 18:56:48 +0530

    remove root files for nextjs app

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Fixing react svg module.
- */
-declare module '*.svg' {
-    import React = require('global');
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
-}
