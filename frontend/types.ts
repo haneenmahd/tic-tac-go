ef6e32c4126263808aca96c62e3e26d05eaecfdf
Haneen Mahdin <haneenmahdin@gmail.com>
2022-12-30 19:55:17 +0530

    chore: add types for `Player`

--- frontend/types.ts
+++ frontend/types.ts
@@ -2,7 +2,16 @@ export type Callback<P, T> = (param: P) => T;
 
 export type CallbackNoParams<T> = () => T;
 
-export type HTTPPort = string | number;
+export interface Player {
+    id: string;
+    name: string;
+    symbol: PlayerSymbol;
+    avatarId: string;
+    score: number;
+    isPlaying: boolean;
+    /** player to start the game */
+    starter: boolean;
+}
 
 export type PlayerSymbol = "X" | "O";
 
